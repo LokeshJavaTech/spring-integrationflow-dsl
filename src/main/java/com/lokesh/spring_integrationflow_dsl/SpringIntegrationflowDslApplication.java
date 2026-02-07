0d6fc26814a3b78ceef6d3880b6391909d8f7701
Lokesh Mittal <lokesh.adonis@gmail.com>
2026-02-06 20:28:41 -0500

    Initial commit

---
+++ src/main/java/com/lokesh/spring_integrationflow_dsl/SpringIntegrationflowDslApplication.java
@@ -0,0 +1,13 @@
+package com.lokesh.spring_integrationflow_dsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationflowDslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationflowDslApplication.class, args);
+	}
+
+}
